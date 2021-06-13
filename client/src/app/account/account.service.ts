e278ceb97d8b76a34c28bae442ef6ca35fdfcbe4
Chiantine Manigos <cmanigos@gmail.com>
2021-06-13 16:07:14 +0800

    188 Creating the login form

---
+++ client/src/app/account/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<IUser>(null!);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.http.post<IUser>(this.baseUrl + 'account/login', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.http.post<IUser>(this.baseUrl + 'account/register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null!);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string) {
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+}
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'} },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'} },
   { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule), data: {breadcrumb: 'Checkout'} },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule), data: {breadcrumb: {skip: true}} },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
