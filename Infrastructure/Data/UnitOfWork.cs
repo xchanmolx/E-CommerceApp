beb673007c9f3c87fc0e27e52ff9b129adcef3ce
Chiantine <cmanigos@gmail.com>
2021-07-16 11:55:46 +0800

    Unit of Work

---
+++ Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _context;
+        private Hashtable _repositories;
+        public UnitOfWork(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> Complete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
